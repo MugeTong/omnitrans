0ee74c97d2b748095a4ce84d95be21fd17a0fc97
MUGE_TONG <here5320@gmail.com>
2025-01-15 12:23:54 +0800

    add tray to the menu

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-import {app, BrowserWindow} from 'electron';
+import {app, BrowserWindow, Tray, Menu} from 'electron';
+import path from 'path';
+import {fileURLToPath} from "node:url";
 
 let mainWindow = null;  // window for the main app
 let omniWindow = null;  // window for the omni box one mini translator
+let tray = null;  // tray icon
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
     mainWindow = new BrowserWindow({
         height: 600,
-        width: 800,
+        width: 1400,
         minHeight: 270,
         minWidth: 400,
         show: false,  // hide the window
         frame: false,  // hide the frame(Title bar)
         autoHideMenuBar: true,  // hide the menu bar
+        backgroundColor: '#000',
     });
     // load the window content
     mainWindow.loadURL("http://localhost:5173/").then(() => {
@@ -31,6 +38,16 @@ const createWindow = () => {
         autoHideMenuBar: true,  // hide the menu bar
     });
 
+
+    tray = new Tray(path.join(__dirname, 'src', 'assets', 'logo.png'));
+    const contextMenu = Menu.buildFromTemplate([
+        { label: '复制应用信息', click: () => {}},
+        { label: '显示窗口', click: () => {}},
+        { label: '退出', click: () => app.quit() },
+    ]);
+    tray.setToolTip('Electron App');
+    tray.setContextMenu(contextMenu);
+
 }
 
 app.whenReady().then(() => {
@@ -40,5 +57,11 @@ app.whenReady().then(() => {
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
+    }
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
     }
 });
